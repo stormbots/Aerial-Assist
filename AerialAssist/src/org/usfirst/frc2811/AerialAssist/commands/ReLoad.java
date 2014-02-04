0f4becede6a0eb3797fb8553469c53661859dd9a
austindb98 <austindbryant@gmail.com>
2014-02-03 19:38:48 -0800

    Fixed big subsystem things

--- AerialAssist/src/org/usfirst/frc2811/AerialAssist/commands/ReLoad.java
+++ AerialAssist/src/org/usfirst/frc2811/AerialAssist/commands/ReLoad.java
@@ -26,7 +26,7 @@ public class ReLoad extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        Robot.shooter.reloadMotors();
+        
     }
 
     // Make this return true when this Command no longer needs to run execute()
