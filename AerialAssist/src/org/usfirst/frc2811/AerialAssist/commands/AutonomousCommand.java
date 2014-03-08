15a6a00b225d65cc252411b2281ed9aacbfa279d
DriverStation <tekdemo+driverstation@github.com>
2014-03-07 19:21:02 -0800

    19:00 working code before autonomous

--- AerialAssist/src/org/usfirst/frc2811/AerialAssist/commands/AutonomousCommand.java
+++ AerialAssist/src/org/usfirst/frc2811/AerialAssist/commands/AutonomousCommand.java
@@ -24,10 +24,12 @@ public class AutonomousCommand extends CommandGroup {
 
     public AutonomousCommand(boolean b) {
         System.out.println("Simple auto command started");
-        
+        //this.addSequential(new JoystickDisable());
+        //this.addSequential(new tempForward(.25));
+        //this.addSequential(new JoystickEnable());
         //this.addSequential(new DriveTimer(2.0));
-//        this.addSequential (new SetToAngle (70));
-//        this.addSequential(new ShootingSequence());
-//        this.addSequential(new SetToZero());
+        //this.addSequential (new SetToAngle (60));
+        //this.addSequential(new ShootingSequence());
+        //this.addSequential(new SetToZero());
    }
 }
--- AerialAssist/src/org/usfirst/frc2811/AerialAssist/commands/tempForward.java
+++ AerialAssist/src/org/usfirst/frc2811/AerialAssist/commands/tempForward.java
@@ -1,4 +1,4 @@
- /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -6,49 +6,46 @@
 package org.usfirst.frc2811.AerialAssist.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import org.usfirst.frc2811.AerialAssist.Robot;
+import org.usfirst.frc2811.AerialAssist.RobotMap;
 
 /**
  *
- * @author Kelson
+ * @author StormBot
  */
-public class ToggleAngle extends Command {
-    public static double curPos;
-    public ToggleAngle() {
+public class tempForward extends Command {
+    public static double timer;
+    public tempForward(double timeout) {
+       timer = timeout; 
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-                System.out.println("SetAngle Start");
-
-       curPos = Robot.lifter2.getPosition();
-        
+        this.setTimeout(timer);
+       
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        //System.out.println(curPos);
-       System.out.println("WTF Toggle Angle " + curPos);
-        if(curPos>22.5){
-            Robot.lifter2.set(0);
-        }
-        else{
-            Robot.lifter2.set(55);
-        }
+        RobotMap.chassisSpeedController1.set(.5);
+        RobotMap.chassisSpeedController2.set(.5);
+        RobotMap.chassisSpeedController3.set(.5);
+        RobotMap.chassisSpeedController4.set(.5);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-                System.out.println("Toggle Angle Finished");
-
-        return true;
+        return this.isTimedOut();
     }
 
     // Called once after isFinished returns true
     protected void end() {
-        
+        RobotMap.chassisSpeedController1.set(0);
+        RobotMap.chassisSpeedController2.set(0);
+        RobotMap.chassisSpeedController3.set(0);
+        RobotMap.chassisSpeedController4.set(0);
+    
     }
 
     // Called when another command which requires one or more of the same
