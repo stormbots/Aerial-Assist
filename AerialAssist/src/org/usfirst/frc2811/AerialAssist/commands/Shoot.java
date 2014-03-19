9545ff6efc6d8b09c8c4d1ffe43e1f9fe648e005
DriverStation <tekdemo+driverstation@github.com>
2014-03-18 18:30:00 -0700

    fixed time bugs
    penguins

--- AerialAssist/src/org/usfirst/frc2811/AerialAssist/commands/Shoot.java
+++ AerialAssist/src/org/usfirst/frc2811/AerialAssist/commands/Shoot.java
@@ -46,7 +46,7 @@ public class Shoot extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return (timeRunning>timeStarted+1  && RobotMap.inPosition.get()!=OI.shooterArmed);
+        return (timeRunning>timeStarted+5  || RobotMap.inPosition.get()!=OI.shooterArmed);
     }
 
     // Called once after isFinished returns true
