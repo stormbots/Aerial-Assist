bfdd0484a6e957fb83199bd7ecc7ada8f580ac84
DriverStation <tekdemo+driverstation@github.com>
2014-03-18 21:13:05 -0700

    autonomous bugs part 2
    continue playing vader's theme
    add more penguins this time

--- AerialAssist/src/org/usfirst/frc2811/AerialAssist/commands/DriveStop.java
+++ AerialAssist/src/org/usfirst/frc2811/AerialAssist/commands/DriveStop.java
@@ -6,42 +6,35 @@
 package org.usfirst.frc2811.AerialAssist.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc2811.AerialAssist.OI;
 import org.usfirst.frc2811.AerialAssist.Robot;
-import org.usfirst.frc2811.AerialAssist.RobotMap;
+
 
 /**
  *
- * @author 128925
+ * @author StormBot
  */
-public class SetToAngle extends Command {
-    double inputangle;
+public class DriveStop extends Command {
     
-    public SetToAngle(double input) {
-        requires(Robot.lifter2);        //TODO determine if not requiring shooter is breaking
-        inputangle=input;
-        //requires(Robot.lifter2);
-    }
-    public SetToAngle() {//Set a default way to call it without angles
-        inputangle=0;
-        //requires(Robot.lifter2); //FIXME Is lifter required breaking things?
+    public DriveStop() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+        requires(Robot.chassis);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        System.out.println("            Setting to angle");
-        Robot.lifter2.set(inputangle);
-        //this.setTimeout(2.0);
-        
+            
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-               System.out.println("still waiting for lifter2 to make it on target");
+        Robot.chassis.manualControl(0, 0);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Robot.lifter2.getOnTarget() ;
+        return OI.stickEngaged;
     }
 
     // Called once after isFinished returns true
